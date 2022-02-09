e5d4afd0cffdeb70042e3458ed006e459a04a15f
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2022-02-09 08:55:59 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <p>TrybeTunes</p>
+        {/* Switch irá reinderizar apenas o primeiro que encontrar, ele é necessário para não reiderizar a notfaund com outro component. */}
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+          {/* NotFound precisa ser colocado por ultimo, caso contrário tudo que estiver abaixo não será reinderizado */}
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NotFound extends React.component {
+  render() {
+    return (
+      <div data-testid="page-not-found" />
+    );
+  }
+}
+
+export default NotFound;
