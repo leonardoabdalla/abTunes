4ac1b5b180ebd5e765753e0f7b8d1b82e7f309aa
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2022-02-12 14:34:28 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,45 @@ import React from 'react';
 import Header from './Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      button: true,
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ name: event.target.value },
+      this.buttonConditions);
+  }
+
+  buttonConditions = () => {
+    const { name } = this.state;
+    const condicao = (name.length >= 2);
+    this.setState({
+      button: !condicao,
+    });
+  }
+
   render() {
+    const { name, button } = this.state;
+
     return (
       <div data-testid="page-search">
-        Search
+        <input
+          data-testid="search-artist-input"
+          value={ name }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ button }
+          onClick={ this.handleChange }
+        >
+          Pesquisar
+        </button>
         <Header />
       </div>
     );
