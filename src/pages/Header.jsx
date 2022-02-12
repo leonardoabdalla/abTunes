5298546ff7a96e58f4cf6c2d271a76bad5d85f23
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2022-02-12 14:00:25 -0300

    Requisito 4

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Carregando from './Carregando';
 
@@ -28,11 +29,16 @@ class Header extends React.Component {
     const { name, loading } = this.state;
     return (
       <header data-testid="header-component">
-        {
-          loading ? <Carregando /> : (
-            <h1 data-testid="header-user-name">{ name }</h1>
-          )
-        }
+        <div>
+          {
+            loading ? <Carregando /> : (
+              <h1 data-testid="header-user-name">{ name }</h1>
+            )
+          }
+          <Link to="/search" data-testid="link-to-search">pesquisa</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">musicas favoritas</Link>
+          <Link to="/profile" data-testid="link-to-profile">exibição de perfil</Link>
+        </div>
       </header>
     );
   }
